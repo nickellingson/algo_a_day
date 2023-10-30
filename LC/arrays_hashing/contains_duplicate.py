73a904f294a87b3610a1dbac854f67abe7096de6
Nicholas Ellingson <nickellingson8@gmail.com>
2023-10-30 15:19:15 -0500

    contains duplicate

---
+++ LC/arrays_hashing/contains_duplicate.py
@@ -0,0 +1,10 @@
+# Use HashSet
+
+class Solution:
+    def containsDuplicate(self, nums):
+        hashset = set()
+        for i in nums:
+            if i in hashset:
+                return True
+            hashset.add(i)
+        return False

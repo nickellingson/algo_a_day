45372b51c5970dda5006c87f28f231f2a4500aaa
Nicholas Ellingson <nickellingson8@gmail.com>
2024-12-14 17:08:22 -0600

    day9

---
+++ adventofcode24/day9/part1.py
@@ -0,0 +1,14 @@
+def read_lines(file):
+    with open(file, "r") as f:
+        for line in f:
+            line_list = line.split()
+            
+
+def helper(line_list):
+    for i in range(len(line_list)):
+        # files
+        if i % 2 == 0:
+            
+
+
+read_lines("example.txt")

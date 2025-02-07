fe0d87c71065f1436e7dae1f447e08634f566ac3
Nicholas Ellingson <nickellingson8@gmail.com>
2025-02-07 16:02:11 -0600

    tree imp

---
+++ implementations/Trees/tree.py
@@ -0,0 +1,49 @@
+from collections import deque
+
+class TreeNode():
+
+    def __init__(self, val):
+        self.val = val
+        self.right = None
+        self.left = None
+
+
+class Tree():
+
+    def __init__(self):
+        self.root = TreeNode(10)
+    
+    def dfs(self, root):
+        # recursive
+        if not root:
+            return None
+        
+        print(root.val)
+        self.dfs(root.left)
+        self.dfs(root.right)
+
+
+    def bfs(self, root):
+        # iterative
+        q = deque()
+        q.append(root)
+        while q:
+            for _ in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+                print(node.val, end="")
+            print("")
+
+if __name__=="__main__":
+    t = Tree()
+    t.root.left = TreeNode(20)
+    t.root.right = TreeNode(30)
+    t.root.left.left = TreeNode(40)
+    t.root.left.right = TreeNode(50)
+    t.root.left.left.left = TreeNode(60)
+
+    t.dfs(t.root)
+    t.bfs(t.root)
--- implementations/Trees/trie.py
+++ implementations/Trees/trie.py
@@ -28,5 +28,10 @@ class Trie():
         return curr.word
     
     def startsWith(self, prefix):
-        pass
+        curr = self.root
+        for c in prefix:
+            if c not in curr.children:
+                return False
+            curr = curr.children[c]
+        return True
 
